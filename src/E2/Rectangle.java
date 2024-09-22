e4b978a4d7c926594729bf78ef7451376024fdca
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-22 17:58:40 +0200

    Declare Rectangle class, extending Shape class

---
+++ src/E2/Rectangle.java
@@ -0,0 +1,24 @@
+package E2;
+
+/**
+ * Represents a Rectangle shape, subclassing Shape.
+ */
+public class Rectangle extends Shape {
+    /**
+     * Creates an instance of Rectangle.
+     * @param width - Width of rectangle.
+     * @param height - Height of rectangle.
+     */
+    public Rectangle(double width, double height) {
+        super(width, height);
+    }
+
+//    /**
+//     * Calculates area of rectangle.
+//     * @return - Area of rectangle.
+//     */
+//    @Override
+//    public double getArea() {
+//        return super.getWidth() * super.getHeight();
+//    }
+}
