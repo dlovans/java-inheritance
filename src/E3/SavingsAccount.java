fcc4322d80372f4d8359f22ec9cf62d2389583d5
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-22 21:19:29 +0200

    Declare SavingsAccount class, subclassing Account class

---
+++ src/E3/SavingsAccount.java
@@ -0,0 +1,34 @@
+package E3;
+
+/**
+ * Represents a savings account, subclassing Account class.
+ */
+public class SavingsAccount extends Account {
+    /**
+     * Interest rate.
+     * @implNote 0.01 rate = 1% interest rate.
+     */
+    final private double interestRate;
+
+    /**
+     * Initializes an instance of SavingsAccount.
+     *
+     * @param rate - Interest rate.
+     * @param balance - Balance in account.
+     * @param name - Name of account owner.
+     *
+     * @implNote 0.01 rate = 1% interest rate.
+     */
+    public SavingsAccount(double rate, double balance, String name) {
+        super(balance, name);
+        this.interestRate = rate;
+    }
+
+    /**
+     * Calculates balance after adding interest.
+     * @return - Balance with interest.
+     */
+    public double addInterest() {
+        return super.balance + super.balance * this.interestRate;
+    }
+}
