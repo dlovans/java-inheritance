9d2d9ae65a253cd48c2a107cd62065e724304671
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-22 17:07:07 +0200

    Declare Animal base class

---
+++ src/E1/Animal.java
@@ -0,0 +1,27 @@
+package E1;
+
+/**
+ * Serves as a base abstract class for all animal types.
+ */
+public abstract class Animal {
+    /**
+     * Name of animal.
+     */
+    String name;
+
+    /**
+     * Creates an instance of Animal.
+     * @param name - Name of animal.
+     */
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Prints what an animal is eating.
+     * @param food - Food item the animal is eating.
+     */
+    public void eat(String food) {
+        System.out.printf("%s eating %s.", this.name, food);
+    }
+}
